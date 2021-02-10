a7d3876535c3e21d9593586892db0bd0288e3ba5
Edwin Amaya <Edwin Amaya@DESKTOP-OKS7EFB>
2021-02-10 17:10:46 -0600

    Se agrego pagina y lista de productos prb

---
+++ FarmaPRueba/FarmaPrueba/Models/ProductoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FarmaPrueba.web.Models
+{
+    public class ProductoModel
+    {
+        public int ID { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
