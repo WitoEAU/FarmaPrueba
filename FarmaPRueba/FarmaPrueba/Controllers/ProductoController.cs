92642705bf2b53c5899603df05b513cc89ee82c7
Edwin Amaya <Edwin Amaya@DESKTOP-OKS7EFB>
2021-02-11 20:22:15 -0600

    Agregue listas y estilos a pagina productos

--- FarmaPRueba/FarmaPrueba/Controllers/ProductoController.cs
+++ FarmaPRueba/FarmaPrueba/Controllers/ProductoController.cs
@@ -1,36 +1,18 @@
-﻿using FarmaPrueba.web.Models;
-using System.Collections.Generic;
+﻿using FarmaPrueba.BL;
 using System.Web.Mvc;
 
 namespace FarmaPrueba.Controllers
 {
     public class ProductoController : Controller
     {
+       
+
         // GET: Producto
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.ID = 1;
-            producto1.Descripcion = "Prd: A";
 
-            var producto2 = new ProductoModel();
-            producto2.ID = 2;
-            producto2.Descripcion = "Prd: B";
-
-            var producto3 = new ProductoModel();
-            producto3.ID = 3;
-            producto3.Descripcion = "Prd: C";
-
-            var producto4 = new ProductoModel();
-            producto4.ID = 4;
-            producto4.Descripcion = "Prd: D";
-
-            var listadeProductos = new List<ProductoModel>();
-
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-            listadeProductos.Add(producto4);
+            var productosBl = new ProductosBL();
+            var listadeProductos = productosBl.ObtenerProductos();
 
 
             return View(listadeProductos);
---
+++ FarmaPRueba/FarmaPueba.BL/ProductosBL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmaPrueba.BL
+{
+    public class ProductosBL
+    {
+      public List<Producto> ObtenerProductos() {
+
+            var producto1 = new Producto();
+            producto1.ID = 1;
+            producto1.Descripcion = "Prd: A";
+            producto1.Precio = 250;
+            
+            var producto2 = new Producto();
+            producto2.ID = 2;
+            producto2.Descripcion = "Prd: B";
+            producto2.Precio = 150;
+
+            var producto3 = new Producto();
+            producto3.ID = 3;
+            producto3.Descripcion = "Prd: C";
+            producto3.Precio = 200;
+
+            var producto4 = new Producto();
+            producto4.ID = 4;
+            producto4.Descripcion = "Prd: D";
+            producto4.Precio = 350;
+
+            var listadeProductos = new List<Producto>();
+
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+            listadeProductos.Add(producto4);
+
+
+
+            return listadeProductos;
+        }
+
+        
+    }
+
+}
