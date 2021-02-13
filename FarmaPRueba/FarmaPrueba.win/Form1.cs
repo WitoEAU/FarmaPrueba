5266b3700f3d6c1a13fa8bdd960fdb68c6b39ddb
Edwin Amaya <Edwin Amaya@DESKTOP-OKS7EFB>
2021-02-13 16:00:32 -0600

    Se instalaron Paquetes NuGet

--- FarmaPRueba/FarmaPrueba.win/Form1.cs
+++ FarmaPRueba/FarmaPrueba.win/Form1.cs
@@ -1,12 +1,5 @@
 ﻿using FarmaPrueba.BL;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FarmaPrueba.win
